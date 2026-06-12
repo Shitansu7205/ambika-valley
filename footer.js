2f975430079021aa23b1dc6bfa6df8d819f1cea5
Shitansu7205 <shitansukumargochhayat@gmail.com>
2026-06-12 16:10:29 +0530

    upoaded

---
+++ footer.js
@@ -0,0 +1,226 @@
+document.write(`
+
+<footer class="footer-top-area footer-top-area-two ">
+			<div class="container">
+				
+				<div class="footer-middle-area pt-60">
+					<div class="row">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<a href="index.html">
+									<img src="assets/ambika-images/logo.jpg" alt="Image" style="height:85px;border-radius:10px;">
+								</a>
+								<ul class="social-icon">
+									<li>
+										<a href="facebook.com/profile.php?id=61590689091857">
+											<i class="bx bxl-facebook"></i>
+										</a>
+									</li>
+									
+									<li>
+										<a href="https://www.instagram.com/invites/contact/?igsh=eimhzmtl4sez&utm_content=fueodkr">
+											<i class="bx bxl-instagram"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Quick Links</h3>
+								<ul>
+									<li>
+										<a href="index.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Home
+										</a>
+									</li>
+									<li>
+										<a href="room.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Rooms
+										</a>
+									</li>
+									<li>
+										<a href="gallery.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											 Gallery
+										</a>
+									</li>
+									
+									
+									<li>
+										<a href="contact.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Contact 
+										</a>
+									</li>
+									<li>
+										<a href="https://ambikavalleyresort938.bookingjini.in/">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Book Now 
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Services</h3>
+								<ul>
+									<li>
+										<a href="services.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Free Wifi
+										</a>
+									</li>
+									<li>
+										<a href="services.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Air Conditioning
+										</a>
+									</li>
+									<li>
+										<a href="services.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Restaurant
+										</a>
+									</li>
+									<li>
+										<a href="services.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											Room Services
+										</a>
+									</li>
+									<li>
+										<a href="hotel-terms.html">
+											<i class="right-icon bx bx-chevrons-right"></i>
+											CCTV Security
+										</a>
+									</li>
+									
+									
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-widget">
+								<h3>Contact Info</h3>
+								<ul class="information">
+									<li class="address">
+										<i class="flaticon-maps-and-flags"></i>
+										<span>Address</span>
+										Swami Ashram, P P, Malegaon, Saputara, Gujarat 394720
+									</li>
+									<li class="address">
+										<i class="flaticon-call"></i>
+										<span>Phone</span>
+										<a href="tel:+919428562202">
+										+91 9428562202
+										</a>
+									</li>
+									<li class="address">
+										<i class="flaticon-envelope"></i>
+										<span>Email</span>
+										<a href="mailto:Ambicavaillyresort@gmail.com">
+                      Ambicavaillyresort@gmail.com
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="footer-bottom-area">
+					<div class="row align-items-center">
+						<div class="col-lg-6">
+							<div class="copy-right">
+								<p>Copyright @2023 | All Rights Reserved</p>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="designed">
+								<p>Designed & Developed By  <a href="https://bookingjini.com/" target="_blank"><img src="assets/img/logo-orange.png" alt="Bookingjini Logo" style="width:130px;"/></a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-shape">
+				<img src="https://www.hotelpahadi.com/assets/img/shape/white-shape-bottom.png" alt="Image">
+			</div>
+		</footer>
+		<!-- Start Go Top Area -->
+		<!-- <div class="go-top">
+			<i class="bx bx-chevrons-up"></i>
+			<i class="bx bx-chevrons-up bx-fade-up"></i>
+		</div> -->
+		<!-- End Go Top Area --> 
+
+        
+      <!-- Jquery Min JS -->
+		  <!-- <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script> -->
+		  <script src="assets/js/jquery.min.js"></script> 
+		  <!-- Bootstrap Bundle Min JS -->
+		  <script src="/assets/js/bootstrap.bundle.min.js"></script>
+		  <!-- Meanmenu Min JS -->
+		  <script src="/assets/js/meanmenu.min.js"></script>
+		  <!-- Owl Carousel Min JS -->
+		  <script src="/assets/js/owl.carousel.min.js"></script>
+		  <!-- Wow Min JS -->
+		  <script src="/assets/js/wow.min.js"></script>
+		  <!-- Nice Select Min JS -->
+		  <script src="/assets/js/nice-select.min.js"></script>
+		  <!-- Magnific Popup Min JS -->
+		  <script src="/assets/js/magnific-popup.min.js"></script>
+		  <!-- Mixitup JS --> 
+		  <script src="/assets/js/jquery.mixitup.min.js"></script>
+		  <!-- Appear Min JS --> 
+		  <script src="/assets/js/appear.min.js"></script>
+		  <!-- Odometer Min JS --> 
+		  <script src="/assets/js/odometer.min.js"></script>
+		  <!-- Datepicker Min JS --> 
+		  <script src="/assets/js/bootstrap-datepicker.min.js"></script>
+		  <!-- Ofi Min JS --> 
+		  <script src="/assets/js/ofi.min.js"></script>
+		  <!-- jarallax Min JS --> 
+		  <script src="/assets/js/jarallax.min.js"></script>
+		  <!-- Form Validator Min JS -->
+		  <script src="/assets/js/form-validator.min.js"></script>
+		  <!-- Contact JS -->
+		  <script src="/assets/js/contact-form-script.js"></script>
+		  <!-- Ajaxchimp Min JS -->
+		  <script src="assets/js/ajaxchimp.min.js"></script>
+		  <!-- Custom JS -->
+		  <script src="/assets/js/custom.js"></script>
+		
+
+ 
+        <!-- Start Instant Booking Widget-->
+            <script>
+            (function() {
+                const addElement = (tag, attributes, parent = document.body) => {
+                    const element = document.createElement(tag);
+                    Object.entries(attributes).forEach(([key, value]) => element.setAttribute(key, value));
+                    parent.appendChild(element);
+                    return element;
+                };
+
+                const rootWidget = addElement("div", {
+                    id:"Mw31A3qlpySYe9FG",
+                    hotelId: 8760,
+                });
+
+                const script = addElement("script", {
+                    src: "https://instant.bookingjini.com/widget.bundle.js",
+                    async: true
+                });
+            })();
+            </script>
+        <!-- End Instant Booking Widget-->
+        
+<script src="https://jiniassist.bookingjini.com/embed.js?company_id=9882"></script>
+
+		
+`)
+
---
+++ header.js
@@ -0,0 +1,107 @@
+document.write(`
+
+<!-- Start Ecorik Navbar Area -->
+<div class="eorik-nav-style fixed-top">
+			<div class="navbar-area">
+				<!-- Menu For Mobile Device -->
+				<div class="mobile-nav">
+					<a href="index.html" class="logo">
+						<img src="assets/ambika-images/logo.jpg" alt="Rourkela Best Hotel" style="border-radius:10px;">
+					</a>
+				</div>
+				<!-- Menu For Desktop Device -->
+				<div class="main-nav">
+					<nav class="navbar navbar-expand-md navbar-light">
+						<div class="container">
+							<a class="navbar-brand" href="index.html
+							">
+								<img src="assets/ambika-images/logo.jpg" alt="hotels in rourkela-Logo" style="height:80px;border-radius:10px;">
+							</a>
+							<div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+								<ul class="navbar-nav m-auto">
+									<li class="nav-item">
+										<a href="index.html" class="nav-link dropdown-toggle active">
+											Home
+											
+										</a>
+										
+									</li>
+									<li class="nav-item">
+										<a href="about.html" class="nav-link dropdown-toggle">
+											About Us
+										</a>
+										
+									</li>
+									<li class="nav-item">
+										<a href="room.html" class="nav-link dropdown-toggle">
+											Rooms
+											
+										</a>
+										
+									</li>
+									<li class="nav-item">
+										<a href="gallery.html" class="nav-link dropdown-toggle">
+											Gallery
+											
+										</a>
+										
+									</li>
+									<li class="nav-item">
+										<a href="services.html" class="nav-link dropdown-toggle">
+											Services
+											
+										</a>
+										
+									</li>
+								
+									<li class="nav-item">
+										<a href="contact.html
+										" class="nav-link dropdown-toggle">
+											Contacts
+											
+										</a>
+										
+									</li>
+
+									<li class="nav-item">
+										<a href="https://ambikavalleyresort938.bookingjini.in/	
+										" class="nav-link dropdown-toggle " style="font-size: 16px;
+    color: #ffffff;
+    padding: 10px 20px 10px 20px;
+    line-height: 1;
+    -webkit-transition: all 0.5s;
+    transition: all 0.5s;
+    z-index: 1;
+    text-transform: capitalize;
+    background-color: #cc8c18;
+    border-radius: 4px;
+    position: relative;
+    cursor: pointer;
+    border-radius: 50px;
+	bottom:5px;
+    font-family: "Muli", sans-serif;
+    text-align: center;"
+	>
+											Book Now
+											
+										</a>
+										
+									</li>
+								</ul>
+								<!-- Start Other Option -->
+								<div class="others-option">
+									<a class="call-us" href="tel:+919428562202">
+										<i class="bx bx-phone-call bx-tada"></i>
+										+91 9428562202
+									</a>
+								</div>
+								<!-- End Other Option -->
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+		<!-- End Ecorik Navbar Area -->
+`)
+
